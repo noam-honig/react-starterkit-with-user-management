94c3997e698650a4da1837620975a29a147e8686
Noam Honig <noam.honig@gmail.com>
2022-05-25 10:19:17 +0300

    todo app tutorial

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,9 +2,16 @@ import express from 'express';
 import { api } from './api';
 import { expressjwt } from 'express-jwt';
 import { getJwtSecret } from '../components/signIn/SignInController';
+import helmet from 'helmet';
+import compression from 'compression';
+import sslRedirect from 'heroku-ssl-redirect';
+import path from 'path';
 
 
 const app = express();
+app.use(sslRedirect());
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(compression());
 app.use(expressjwt({
     secret: getJwtSecret(),
     credentialsRequired: false,
@@ -12,4 +19,9 @@ app.use(expressjwt({
 }));
 app.use(api);
 
-app.listen(3002, () => console.log("Server started"));
+app.use(express.static(path.join(__dirname, '../')));
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, '../', 'index.html'));
+});
+
+app.listen(process.env["PORT"] || 3002, () => console.log("Server started"));
